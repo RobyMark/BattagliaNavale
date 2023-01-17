160ca4e289c144833c3df244373f24fd832142cc
Salvatore <salvatoredipietro35@gmail.com>
2023-01-17 21:05:42 +0100

    feat: file added to the brunch

--- tests/test_user.py
+++ tests/test_user.py
@@ -6,8 +6,14 @@ from pytest_mock import MockerFixture
 
 def test_placeShips() -> None:
     grid = Grid(4,4)
-    user = User(grid)
+    user = User()
     user.placeShips(grid,[2,2])
-    mocker.patch.object(input, "input", return_value="2")
-    assert grid.cells[2,2] == "ships"
+    #mocker.patch.object(input, "input", return_value="2")
+    assert grid.cells[2][2] == "ship"
+
+def test_userMove() -> None:
+    grid = Grid(4,4)
+    user = User()
+    user.userMove(grid)
+    assert grid.cells[2][2] in ("miss", "hit")
 
