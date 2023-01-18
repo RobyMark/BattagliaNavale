846ded1b1fbaadb4660c7578cdea0e108ee391fc
RobyMark <rob_marchese@virgilio.it>
2023-01-18 16:37:18 +0100

    fix(test_pair)

--- tests/test_pair.py
+++ tests/test_pair.py
@@ -1,14 +1,14 @@
 from src.pair import Pair
 
-def test_pair()
+def test_pair() -> None:
     p = Pair(1, 5)
     assert p.x == 1
-    assert p.x == 5
+    assert p.y == 5
 
-def test_pairX()
+def test_pairX() -> None:
     p = Pair(1, 5)
     assert p.getX() == 1
 
-def test_pairX()
+def test_pairX() -> None:
     p = Pair(1, 5)
     assert p.getY() == 5
