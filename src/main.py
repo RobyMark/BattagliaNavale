251d836aad49bfdff88c4249a8f3b1661f77ad1a
Salvatore <salvatoredipietro35@gmail.com>
2023-01-18 10:50:08 +0100

    feat: add first calls of functions into main

--- src/main.py
+++ src/main.py
@@ -4,3 +4,14 @@ from src.grid import Grid
 from src.user import User
 from src.ai import AI
 
+def main():
+    grid = Grid()
+    user = User()
+    ai = AI()
+
+    ai.placeShips(grid)
+    
+
+
+if __name__ == "__main__":
+    main()
