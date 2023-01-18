4a4c319df5d9f1f79623313fd2dd43d9510f19e0
Salvatore <salvatoredipietro35@gmail.com>
2023-01-18 12:06:05 +0100

    feat: add a if to check if win user or ai

--- src/main.py
+++ src/main.py
@@ -14,6 +14,12 @@ def main():
     while (gridAi.checkLenghtShip() != 0 and gridUs.checkLenghtShip() != 0):
         user.userMove(gridUs)
         ai.makeMove(gridAi)
+
+    if (gridAi.checkLenghtShip() == 0):
+        print("You win!")
+    else :
+        print("You lose...")
+    
         
 
 if __name__ == "__main__":
